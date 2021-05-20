91f6e6a63d336b727b5f627e36cde40db6b49d98
L0fty7073 <raffa2001ciao@hotmail.com>
2021-05-20 22:59:46 +0200

    modifiche ambiente

--- FrontEnd/Contenuti/Script/Monitoraggio.js
+++ FrontEnd/Contenuti/Script/Monitoraggio.js
@@ -1,35 +1,28 @@
 // JavaScript Document
-var Tirreno = document.getElementById("tirreno");
-var Ionio = document.getElementById("ionio");
-var Adriatico= document.getElementById("adriatico");
-var Ligure = document.getElementById("ligure");
+
 
 function mostratir(){
-	window.location("MonitoraggioAmbiente.html#Tirreno");
-	Tirreno.style.display ="block";
-	Ionio.style.display ="none";
-	Adriatico.style.display ="none";
-	Ligure.style.display ="none";
+	document.getElementById("tirreno").style.display ="block";
+	document.getElementById("ionio").style.display ="none";
+	document.getElementById("adriatico").style.display ="none";
+	document.getElementById("ligure").style.display ="none";
 }
 function mostraion(){
-	window.location("MonitoraggioAmbiente.html#Ionio");
-	Tirreno.style.display ="none";
-	Ionio.style.display ="block";
-	Adriatico.style.display ="none";
-	Ligure.style.display ="none";
+	document.getElementById("tirreno").style.display ="none";
+	document.getElementById("ionio").style.display ="block";
+	document.getElementById("adriatico").style.display ="none";
+	document.getElementById("ligure").style.display ="none";
 }
 function mostraadr(){
-	window.location("MonitoraggioAmbiente.html#Adriatico");
-	Tirreno.style.display ="none";
-	Ionio.style.display ="none";
-	Adriatico.style.display ="block";
-	Ligure.style.display ="none";
+	document.getElementById("tirreno").style.display ="none";
+	document.getElementById("ionio").style.display ="none";
+	document.getElementById("adriatico").style.display ="block";
+	document.getElementById("ligure").style.display ="none";
 }
 function mostralig(){
-	window.location("MonitoraggioAmbiente.html#Ligure");
-	Tirreno.style.display ="none";
-	Ionio.style.display ="none";
-	Adriatico.style.display ="none";
-	Ligure.style.display ="block";
+	document.getElementById("tirreno").style.display ="none";
+	document.getElementById("ionio").style.display ="none";
+	document.getElementById("adriatico").style.display ="none";
+	document.getElementById("ligure").style.display ="block";
 }
 
