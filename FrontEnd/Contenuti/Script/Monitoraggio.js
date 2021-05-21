f6be4ab134f31b3344e0e5f44f4734513ed38253
L0fty7073 <raffa2001ciao@hotmail.com>
2021-05-21 13:19:22 +0200

    modifiche ambiente

--- FrontEnd/Contenuti/Script/Monitoraggio.js
+++ FrontEnd/Contenuti/Script/Monitoraggio.js
@@ -2,27 +2,29 @@
 
 
 function mostratir(){
+	alert("ciao");
+	window.open("MonitoraggioAmbiente.html", "_self");
 	document.getElementById("tirreno").style.display ="block";
 	document.getElementById("ionio").style.display ="none";
 	document.getElementById("adriatico").style.display ="none";
-	document.getElementById("ligure").style.display ="none";
+	document.getElementById("ligure").style.display ="none";	
 }
 function mostraion(){
-	document.getElementById("tirreno").style.display ="none";
-	document.getElementById("ionio").style.display ="block";
-	document.getElementById("adriatico").style.display ="none";
-	document.getElementById("ligure").style.display ="none";
+	document.getElementById("tirreno").style.visibility ="hidden";
+	document.getElementById("ionio").style.visibility ="visible";
+	document.getElementById("adriatico").style.visibility ="hidden";
+	document.getElementById("ligure").style.visibility ="hidden";
 }
 function mostraadr(){
-	document.getElementById("tirreno").style.display ="none";
-	document.getElementById("ionio").style.display ="none";
-	document.getElementById("adriatico").style.display ="block";
-	document.getElementById("ligure").style.display ="none";
+	document.getElementById("tirreno").style.visibility ="hidden";
+	document.getElementById("ionio").style.visibility ="hidden";
+	document.getElementById("adriatico").style.visibility ="visible";
+	document.getElementById("ligure").style.visibility ="hidden";
 }
 function mostralig(){
-	document.getElementById("tirreno").style.display ="none";
-	document.getElementById("ionio").style.display ="none";
-	document.getElementById("adriatico").style.display ="none";
-	document.getElementById("ligure").style.display ="block";
+	document.getElementById("tirreno").style.visibility ="hidden";
+	document.getElementById("ionio").style.visibility ="hidden";
+	document.getElementById("adriatico").style.visibility ="hidden";
+	document.getElementById("ligure").style.visibility ="visible";
 }
 
