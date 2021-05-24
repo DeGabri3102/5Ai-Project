5c0a56da03fc6ceb4c92420326581203ea93cef7
L0fty7073 <raffa2001ciao@hotmail.com>
2021-05-24 11:23:18 +0200

    modifiche ambiente

--- FrontEnd/Contenuti/Script/Monitoraggio.js
+++ FrontEnd/Contenuti/Script/Monitoraggio.js
@@ -2,29 +2,27 @@
 
 
 function mostratir(){
-	alert("ciao");
-	window.open("MonitoraggioAmbiente.html", "_self");
 	document.getElementById("tirreno").style.display ="block";
 	document.getElementById("ionio").style.display ="none";
 	document.getElementById("adriatico").style.display ="none";
 	document.getElementById("ligure").style.display ="none";	
 }
 function mostraion(){
-	document.getElementById("tirreno").style.visibility ="hidden";
-	document.getElementById("ionio").style.visibility ="visible";
-	document.getElementById("adriatico").style.visibility ="hidden";
-	document.getElementById("ligure").style.visibility ="hidden";
+	document.getElementById("tirreno").style.display ="none";
+	document.getElementById("ionio").style.display ="block";
+	document.getElementById("adriatico").style.display ="none";
+	document.getElementById("ligure").style.display ="none";
 }
 function mostraadr(){
-	document.getElementById("tirreno").style.visibility ="hidden";
-	document.getElementById("ionio").style.visibility ="hidden";
-	document.getElementById("adriatico").style.visibility ="visible";
-	document.getElementById("ligure").style.visibility ="hidden";
+	document.getElementById("tirreno").style.display ="none";
+	document.getElementById("ionio").style.display ="none";
+	document.getElementById("adriatico").style.display ="block";
+	document.getElementById("ligure").style.display ="none";
 }
 function mostralig(){
-	document.getElementById("tirreno").style.visibility ="hidden";
-	document.getElementById("ionio").style.visibility ="hidden";
-	document.getElementById("adriatico").style.visibility ="hidden";
-	document.getElementById("ligure").style.visibility ="visible";
+	document.getElementById("tirreno").style.display ="none";
+	document.getElementById("ionio").style.display ="none";
+	document.getElementById("adriatico").style.display ="none";
+	document.getElementById("ligure").style.display ="block";
 }
 
