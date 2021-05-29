3b2234534b50a2f71d4e6f159b82954db2314037
SalvoGeraci11 <salvogeraci2003@gmail.com>
2021-05-29 23:42:31 +0200

    Aggiunte tutte le navi nel database

--- Database/dump_DataBase.sql
+++ Database/dump_DataBase.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versione server:              10.4.18-MariaDB - mariadb.org binary distribution
--- S.O. server:                  Win64
+-- Versione server:              10.1.37-MariaDB - mariadb.org binary distribution
+-- S.O. server:                  Win32
 -- HeidiSQL Versione:            11.2.0.6213
 -- --------------------------------------------------------
 
@@ -65,21 +65,20 @@ CREATE TABLE IF NOT EXISTS `amministratori` (
   `iDAmministratore` tinyint(4) NOT NULL AUTO_INCREMENT,
   `nome` varchar(20) DEFAULT NULL,
   `cognome` varchar(15) DEFAULT NULL,
-  `email` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  PRIMARY KEY (`iDAmministratore`),
-  UNIQUE KEY `email` (`email`)
+  `email` varchar(40) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`iDAmministratore`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 -- Dump dei dati della tabella charternautico.amministratori: ~6 rows (circa)
 /*!40000 ALTER TABLE `amministratori` DISABLE KEYS */;
 INSERT INTO `amministratori` (`iDAmministratore`, `nome`, `cognome`, `email`, `password`) VALUES
-	(1, 'Giovanni', 'Aleo', 'giovanni@bhor.it', 'giovanni'),
-	(2, 'Roberto', 'Stella', 'roberto@bhor.it', 'roberto'),
-	(3, 'Antonio', 'Marca', 'antonio@bhor.it', 'antonio'),
-	(4, 'Riccardo', 'Chiesa', 'riccardo@bhor.it', 'riccardo'),
-	(5, 'Simone', 'Torrisi', 'simone@bhor.it', 'simone'),
-	(6, 'Salvo', 'Castagna', 'salvo@bhor.it', 'salvo');
+	(1, 'Giovanni', 'Aleo', 'giovannialeo27@gmail.com', 'giovannialeo'),
+	(2, 'Roberto', 'Stella', 'robertostella@gmail.com', 'robertostella'),
+	(3, 'Antonio', 'Marca', 'antoniomarca@gmail.com', 'antoniomarca'),
+	(4, 'Riccardo', 'Chiesa', 'riccardochiesa@gmail.com', 'riccardochiesa'),
+	(5, 'Simone', 'Torrisi', 'simonetorrisi@gmail.com', 'simonetorrisi'),
+	(6, 'Salvo', 'Castagna', 'salvocastagna@gmail.com', 'salvocastagna');
 /*!40000 ALTER TABLE `amministratori` ENABLE KEYS */;
 
 -- Dump della struttura di tabella charternautico.centraline
@@ -88,9 +87,9 @@ CREATE TABLE IF NOT EXISTS `centraline` (
   `x` double DEFAULT NULL,
   `y` double DEFAULT NULL,
   PRIMARY KEY (`iDCentralina`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 
--- Dump dei dati della tabella charternautico.centraline: ~6 rows (circa)
+-- Dump dei dati della tabella charternautico.centraline: ~36 rows (circa)
 /*!40000 ALTER TABLE `centraline` DISABLE KEYS */;
 INSERT INTO `centraline` (`iDCentralina`, `x`, `y`) VALUES
 	(1, 100, 100),
@@ -98,7 +97,37 @@ INSERT INTO `centraline` (`iDCentralina`, `x`, `y`) VALUES
 	(3, 27, 27),
 	(4, 70, 100),
 	(5, 190, 280),
-	(6, 300, 100);
+	(6, 300, 100),
+	(7, 220, 49),
+	(8, 300, 28),
+	(9, 270, 300),
+	(10, 120, 145),
+	(11, 170, 202),
+	(12, 190, 110),
+	(13, 300, 230),
+	(14, 100, 290),
+	(15, 320, 110),
+	(16, 220, 123),
+	(17, 340, 230),
+	(18, 140, 150),
+	(19, 230, 140),
+	(20, 320, 340),
+	(21, 102, 256),
+	(22, 257, 189),
+	(23, 320, 237),
+	(24, 430, 280),
+	(25, 150, 200),
+	(26, 410, 230),
+	(27, 200, 380),
+	(28, 290, 370),
+	(29, 234, 338),
+	(30, 360, 290),
+	(31, 248, 179),
+	(32, 250, 320),
+	(33, 189, 834),
+	(34, 127, 272),
+	(35, 377, 137),
+	(36, 287, 340);
 /*!40000 ALTER TABLE `centraline` ENABLE KEYS */;
 
 -- Dump della struttura di tabella charternautico.clienti
@@ -108,28 +137,27 @@ CREATE TABLE IF NOT EXISTS `clienti` (
   `nome` varchar(20) DEFAULT NULL,
   `cognome` varchar(15) DEFAULT NULL,
   `ddn` date DEFAULT NULL,
-  `email` varchar(40) NOT NULL,
-  `password` varchar(20) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
   `nTel` varchar(15) DEFAULT NULL,
   `indirizzo` varchar(30) DEFAULT NULL,
   `numeroCivico` varchar(3) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
   `patentenautica` enum('SI','NO') DEFAULT NULL,
-  PRIMARY KEY (`codDocumento`),
-  UNIQUE KEY `UNIQUE` (`email`) USING BTREE
+  PRIMARY KEY (`codDocumento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dump dei dati della tabella charternautico.clienti: ~4 rows (circa)
 /*!40000 ALTER TABLE `clienti` DISABLE KEYS */;
-INSERT INTO `clienti` (`codDocumento`, `tipoDocumento`, `nome`, `cognome`, `ddn`, `email`, `password`, `nTel`, `indirizzo`, `numeroCivico`, `patentenautica`) VALUES
-	('2727009', 'Patente nautica', 'Emanuele', 'Porto', '1969-09-27', 'emanueleporto@gmail.com', 'emanueleporto', '3272708633', 'Via Europa', '10', 'SI'),
-	('AB1275467', 'Passaporto', 'Gabriele', 'Testa', '1980-01-01', 'gabrieletesta@gmail.com', 'gabrieletesta', '3402586710', 'Via Verdi', '45', 'SI'),
-	('HPFPYM47A27D408H', 'Carta d identità', 'Francesco', 'Russo', '1960-07-01', 'francescorusso@gmail.com', 'francescorusso', '3295607104', 'Via Vittorio Emanuele', '66', 'SI'),
-	('TZMKSF54L46I711U', 'Carta d identità', 'Antonio', 'Di Mauro', '1996-12-10', 'antoniodimauro@gmail.com', 'antoniodimauro', '334437723', 'Via Rossi', '93', 'NO');
+INSERT INTO `clienti` (`codDocumento`, `tipoDocumento`, `nome`, `cognome`, `ddn`, `email`, `nTel`, `indirizzo`, `numeroCivico`, `password`, `patentenautica`) VALUES
+	('2727009', 'Patente nautica', 'Emanuele', 'Porto', '1969-09-27', 'emanueleporto@gmail.com', '3272708633', 'Via Europa', '10', 'emanueleporto', 'SI'),
+	('AB1275467', 'Passaporto', 'Gabriele', 'Testa', '1980-01-01', 'gabrieletesta@gmail.com', '3402586710', 'Via Verdi', '45', 'gabrieletesta', 'SI'),
+	('HPFPYM47A27D408H', 'Carta d identità', 'Francesco', 'Russo', '1960-07-01', 'francescorusso@gmail.com', '3295607104', 'Via Vittorio Emanuele', '66', 'francescorusso', 'SI'),
+	('TZMKSF54L46I711U', 'Carta d identità', 'Antonio', 'Di Mauro', '1996-12-10', 'antoniodimauro@gmail.com', '334437723', 'Via Rossi', '93', 'antoniodimauro', 'NO');
 /*!40000 ALTER TABLE `clienti` ENABLE KEYS */;
 
 -- Dump della struttura di tabella charternautico.comunicazioni
 CREATE TABLE IF NOT EXISTS `comunicazioni` (
-  `iDComunicazione` varchar(5) NOT NULL,
+  `iDComunicazione` smallint(6) NOT NULL AUTO_INCREMENT,
   `iDCentralina` tinyint(4) DEFAULT NULL,
   `iDPorto` tinyint(4) DEFAULT NULL,
   `d` datetime DEFAULT NULL,
@@ -138,18 +166,18 @@ CREATE TABLE IF NOT EXISTS `comunicazioni` (
   KEY `iDPorto` (`iDPorto`),
   CONSTRAINT `comunicazioni_ibfk_1` FOREIGN KEY (`iDCentralina`) REFERENCES `centraline` (`iDCentralina`),
   CONSTRAINT `comunicazioni_ibfk_2` FOREIGN KEY (`iDPorto`) REFERENCES `porti` (`iDPorto`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- Dump dei dati della tabella charternautico.comunicazioni: ~7 rows (circa)
 /*!40000 ALTER TABLE `comunicazioni` DISABLE KEYS */;
 INSERT INTO `comunicazioni` (`iDComunicazione`, `iDCentralina`, `iDPorto`, `d`) VALUES
-	('1', 2, 6, '2021-05-10 13:27:06'),
-	('2', 6, 6, '2021-05-10 13:27:20'),
-	('3', 1, 3, '2021-04-10 13:27:32'),
-	('4', 3, 1, '2021-05-02 13:27:00'),
-	('5', 4, 2, '2021-05-10 13:28:49'),
-	('6', 5, 5, '2021-04-19 13:29:16'),
-	('7', 1, 4, '2021-01-27 13:29:42');
+	(1, 2, 6, '2021-05-10 13:27:06'),
+	(2, 6, 6, '2021-05-10 13:27:20'),
+	(3, 1, 3, '2021-04-10 13:27:32'),
+	(4, 3, 1, '2021-05-02 13:27:00'),
+	(5, 4, 2, '2021-05-10 13:28:49'),
+	(6, 5, 5, '2021-04-19 13:29:16'),
+	(7, 1, 4, '2021-01-27 13:29:42');
 /*!40000 ALTER TABLE `comunicazioni` ENABLE KEYS */;
 
 -- Dump della struttura di tabella charternautico.imbarcazioni
@@ -167,27 +195,57 @@ CREATE TABLE IF NOT EXISTS `imbarcazioni` (
   PRIMARY KEY (`iDImb`),
   KEY `iDCentralina` (`iDCentralina`),
   CONSTRAINT `imbarcazioni_ibfk_1` FOREIGN KEY (`iDCentralina`) REFERENCES `centraline` (`iDCentralina`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 
--- Dump dei dati della tabella charternautico.imbarcazioni: ~6 rows (circa)
+-- Dump dei dati della tabella charternautico.imbarcazioni: ~36 rows (circa)
 /*!40000 ALTER TABLE `imbarcazioni` DISABLE KEYS */;
 INSERT INTO `imbarcazioni` (`iDImb`, `iDCentralina`, `nome`, `marca`, `modello`, `prezzonoleggio_al_giorno`, `lunghezza`, `potenza_cv`, `nPostiLetto`, `obbligoPatenteNautica`) VALUES
-	(1, 2, 'Dea', 'Apreamare', 'Luxury', 250, 60, '100', '10', 'SI'),
-	(2, 3, 'Nox', 'Dufour', 'Luxury', 200, 40, '90', '10', 'SI'),
-	(3, 1, 'Mea', 'Cruisers Yachts', 'Cruiser', 160, 35, '70', '8', 'SI'),
-	(4, 6, 'Drog', 'Doral', 'Cruiser', 125, 32, '65', '8', 'SI'),
+	(1, 1, 'Dea', 'Apreamare', 'Luxury', 250, 60, '100', '10', 'SI'),
+	(2, 2, 'Nox', 'Dufour', 'Luxury', 200, 52, '90', '10', 'SI'),
+	(3, 3, 'Mea', 'Cruisers Yachts', 'Cruiser', 160, 45, '70', '8', 'SI'),
+	(4, 4, 'Drog', 'Doral', 'Cruiser', 125, 35, '65', '8', 'SI'),
 	(5, 5, 'Baste', 'Houseboat', 'Economy', 100, 25, '35', '6', 'NO'),
-	(6, 4, 'Mors', 'Oyster Marine', 'Economy', 115, 25, '35', '6', 'NO');
+	(6, 6, 'Mors', 'Oyster Marine', 'Economy', 115, 25, '35', '6', 'NO'),
+	(7, 7, 'Athena\r\n', 'Apreamare', 'Luxury', 250, 55, '110', '10', 'SI'),
+	(8, 8, 'Poseidon', 'Dufour', 'Luxury', 240, 50, '95', '10', 'SI'),
+	(9, 9, 'Nautilus', 'Apreamare', 'Luxury', 230, 53, '100', '10', 'SI'),
+	(10, 10, 'Fenrir', 'Dufour', 'Luxury', 210, 56, '90', '10', 'SI'),
+	(11, 11, 'Freia', 'Apreamare', 'Luxury', 200, 53, '90', '10', 'SI'),
+	(12, 12, 'Loki', 'Dufour', 'Luxury', 220, 48, '95', '10', 'SI'),
+	(13, 13, 'Thor', 'Apreamare', 'Luxury', 230, 50, '100', '10', 'SI'),
+	(14, 14, 'Nasus', 'Dufour', 'Luxury', 210, 55, '110', '10', 'SI'),
+	(15, 15, 'Renekton', 'Apreamare', 'Luxury', 200, 55, '85', '10', 'SI'),
+	(16, 16, 'Ani', 'Dufour', 'Luxury', 210, 50, '80', '10', 'SI'),
+	(17, 17, 'Atalanta', 'Cruisers Yachts', 'Cruiser', 140, 43, '75', '8', 'SI'),
+	(18, 18, 'Caronte', 'Doral', 'Cruiser', 130, 40, '70', '8', 'SI'),
+	(19, 19, 'Cassandra', 'Cruisers Yachts', 'Cruiser', 150, 45, '65', '8', 'SI'),
+	(20, 20, 'Circe', 'Doral', 'Cruiser', 165, 43, '63', '8', 'SI'),
+	(21, 21, 'Elena', 'Cruisers Yachts', 'Cruiser', 130, 37, '68', '8', 'SI'),
+	(22, 22, 'Europa', 'Doral', 'Cruiser', 140, 38, '72', '8', 'SI'),
+	(23, 23, 'Mida', 'Cruisers Yachts', 'Cruiser', 136, 40, '74', '8', 'SI'),
+	(24, 24, 'Mirmidone', 'Doral', 'Cruiser', 150, 36, '65', '8', 'SI'),
+	(25, 25, 'Odisseo', 'Cruisers Yachts', 'Cruiser', 165, 42, '78', '8', 'SI'),
+	(26, 26, 'Sisifo', 'Doral', 'Cruiser', 150, 42, '74', '8', 'SI'),
+	(27, 27, 'Marco', 'Houseboat', 'Economy', 110, 25, '35', '6', 'NO'),
+	(28, 28, 'Enzuccio', 'Oyster Marine', 'Economy', 120, 30, '38', '6', 'NO'),
+	(29, 29, 'Martina', 'Houseboat', 'Economy', 100, 28, '32', '6', 'NO'),
+	(30, 30, 'Agata', 'Oyster Marine', 'Economy', 98, 26, '30', '6', 'NO'),
+	(31, 31, 'Angelo', 'Houseboat', 'Economy', 112, 24, '39', '6', 'NO'),
+	(32, 32, 'Mirko', 'Oyster Marine', 'Economy', 105, 25, '38', '6', 'NO'),
+	(33, 33, 'Riccardo', 'Houseboat', 'Economy', 100, 22, '37', '6', 'NO'),
+	(34, 34, 'Salvo', 'Oyster Marine', 'Economy', 113, 26, '34', '6', 'NO'),
+	(35, 35, 'Gabriele', 'Houseboat', 'Economy', 116, 24, '36', '6', 'NO'),
+	(36, 36, 'Tano', 'Oyster Marine', 'Economy', 120, 28, '38', '6', 'NO');
 /*!40000 ALTER TABLE `imbarcazioni` ENABLE KEYS */;
 
 -- Dump della struttura di tabella charternautico.monitoraggi
 CREATE TABLE IF NOT EXISTS `monitoraggi` (
-  `iDMonitoraggio` varchar(5) NOT NULL,
+  `iDMonitoraggio` smallint(6) NOT NULL AUTO_INCREMENT,
   `nomeZona` enum('Mar Tirreno','Costiera Amalfitana','Delta del Po','Mar Ligure','Parco delle cinque terre','Mar Ionio','Costa Smeralda') DEFAULT NULL,
   `iDCentralina` tinyint(4) DEFAULT NULL,
   `ph` double DEFAULT NULL,
   `temperatura` double DEFAULT NULL,
-  `conducibilità` double DEFAULT NULL,
+  `conducibilita` smallint(6) DEFAULT NULL,
   `redox` double DEFAULT NULL,
   `ossigeno` double DEFAULT NULL,
   `torbidita` double DEFAULT NULL,
@@ -197,24 +255,24 @@ CREATE TABLE IF NOT EXISTS `monitoraggi` (
   KEY `iDCentralina` (`iDCentralina`),
   CONSTRAINT `monitoraggi_ibfk_1` FOREIGN KEY (`nomeZona`) REFERENCES `acque` (`nomeZona`),
   CONSTRAINT `monitoraggi_ibfk_2` FOREIGN KEY (`iDCentralina`) REFERENCES `centraline` (`iDCentralina`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- Dump dei dati della tabella charternautico.monitoraggi: ~8 rows (circa)
 /*!40000 ALTER TABLE `monitoraggi` DISABLE KEYS */;
-INSERT INTO `monitoraggi` (`iDMonitoraggio`, `nomeZona`, `iDCentralina`, `ph`, `temperatura`, `conducibilità`, `redox`, `ossigeno`, `torbidita`, `d`) VALUES
-	('1', 'Mar Tirreno', 5, 7.3, 25, 53, 350, 5.3, 6, '2021-05-13 10:31:53'),
-	('2', 'Costiera Amalfitana', 1, 7.5, 26, 51.2, 390, 8.1, 6.3, '2021-05-11 10:32:15'),
-	('3', 'Delta del Po', 3, 7.6, 29, 50.9, 345, 4.9, 8, '2021-05-03 10:32:35'),
-	('4', 'Mar Ionio', 1, 8, 24, 52.6, 360, 5, 7.2, '2021-05-13 11:24:18'),
-	('5', 'Costa Smeralda', 6, 7.9, 23, 53, 375, 6.1, 5.9, '2021-05-10 11:24:19'),
-	('6', 'Mar Ligure', 4, 6.9, 25, 50, 370, 6.9, 6.7, '2021-05-02 11:24:27'),
-	('7', 'Parco delle cinque terre', 2, 6.9, 26, 51.3, 365, 6, 7, '2021-05-04 11:24:36'),
-	('8', 'Mar Ionio', 6, 7.6, 27, 53.1, 350, 7, 8, '2021-04-18 11:24:48');
+INSERT INTO `monitoraggi` (`iDMonitoraggio`, `nomeZona`, `iDCentralina`, `ph`, `temperatura`, `conducibilita`, `redox`, `ossigeno`, `torbidita`, `d`) VALUES
+	(1, 'Mar Tirreno', 5, 7.3, 25, 53, 350, 5.3, 6, '2021-05-13 10:31:53'),
+	(2, 'Costiera Amalfitana', 1, 7.5, 26, 51, 390, 8.1, 6.3, '2021-05-11 10:32:15'),
+	(3, 'Delta del Po', 3, 7.6, 29, 51, 345, 4.9, 8, '2021-05-03 10:32:35'),
+	(4, 'Mar Ionio', 1, 8, 24, 53, 360, 5, 7.2, '2021-05-13 11:24:18'),
+	(5, 'Costa Smeralda', 6, 7.9, 23, 53, 375, 6.1, 5.9, '2021-05-10 11:24:19'),
+	(6, 'Mar Ligure', 4, 6.9, 25, 50, 370, 6.9, 6.7, '2021-05-02 11:24:27'),
+	(7, 'Parco delle cinque terre', 2, 6.9, 26, 51, 365, 6, 7, '2021-05-04 11:24:36'),
+	(8, 'Mar Ionio', 6, 7.6, 27, 53, 350, 7, 8, '2021-04-18 11:24:48');
 /*!40000 ALTER TABLE `monitoraggi` ENABLE KEYS */;
 
 -- Dump della struttura di tabella charternautico.ormeggi
 CREATE TABLE IF NOT EXISTS `ormeggi` (
-  `iDOrmeggio` varchar(5) NOT NULL,
+  `iDOrmeggio` smallint(6) NOT NULL AUTO_INCREMENT,
   `iDPorto` tinyint(4) DEFAULT NULL,
   `iDImb` tinyint(4) DEFAULT NULL,
   `dataOrmeggio` datetime DEFAULT NULL,
@@ -223,16 +281,47 @@ CREATE TABLE IF NOT EXISTS `ormeggi` (
   KEY `iDImb` (`iDImb`),
   CONSTRAINT `ormeggi_ibfk_1` FOREIGN KEY (`iDPorto`) REFERENCES `porti` (`iDPorto`),
   CONSTRAINT `ormeggi_ibfk_2` FOREIGN KEY (`iDImb`) REFERENCES `imbarcazioni` (`iDImb`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 -- Dump dei dati della tabella charternautico.ormeggi: ~5 rows (circa)
 /*!40000 ALTER TABLE `ormeggi` DISABLE KEYS */;
 INSERT INTO `ormeggi` (`iDOrmeggio`, `iDPorto`, `iDImb`, `dataOrmeggio`) VALUES
-	('1', 6, 1, '2021-05-13 10:47:27'),
-	('2', 6, 2, '2021-05-03 10:47:57'),
-	('3', 1, 5, '2021-05-10 10:47:59'),
-	('4', 2, 3, '2021-04-13 10:48:46'),
-	('5', 5, 6, '2021-04-03 10:49:05');
+	(1, 6, 1, '2021-05-13 10:47:27'),
+	(2, 6, 2, '2021-05-03 10:47:57'),
+	(3, 1, 5, '2021-05-10 10:47:59'),
+	(4, 2, 3, '2021-04-13 10:48:46'),
+	(5, 5, 6, '2021-04-03 10:49:05'),
+	(6, 1, 27, '2021-05-29 23:11:26'),
+	(7, 1, 16, '2021-05-29 23:12:15'),
+	(8, 1, 11, '2021-05-29 23:12:14'),
+	(9, 1, 21, '2021-05-29 23:13:02'),
+	(10, 1, 22, '2021-05-29 23:13:13'),
+	(11, 2, 4, '2021-05-29 23:13:44'),
+	(12, 2, 10, '2021-05-29 23:14:13'),
+	(13, 2, 9, '2021-05-29 23:14:26'),
+	(14, 2, 35, '2021-05-29 23:14:38'),
+	(15, 2, 34, '2021-05-29 23:14:51'),
+	(16, 3, 15, '2021-05-29 23:15:19'),
+	(17, 3, 14, '2021-05-29 23:15:37'),
+	(18, 3, 26, '2021-05-29 23:15:53'),
+	(19, 3, 25, '2021-05-29 23:16:02'),
+	(20, 3, 33, '2021-05-29 23:16:26'),
+	(21, 3, 32, '2021-05-29 23:16:42'),
+	(22, 4, 13, '2021-05-29 23:17:05'),
+	(23, 4, 12, '2021-05-29 23:17:15'),
+	(24, 4, 24, '2021-05-29 23:17:29'),
+	(25, 4, 23, '2021-05-29 23:17:51'),
+	(26, 4, 31, '2021-05-29 23:18:08'),
+	(27, 4, 30, '2021-05-29 23:18:39'),
+	(28, 5, 36, '2021-05-29 23:19:32'),
+	(29, 5, 8, '2021-05-29 23:19:50'),
+	(30, 5, 7, '2021-05-29 23:20:04'),
+	(31, 5, 20, '2021-05-29 23:20:19'),
+	(32, 6, 28, '2021-05-29 23:20:30'),
+	(33, 5, 19, '2021-05-29 23:20:47'),
+	(34, 6, 17, '2021-05-29 23:21:21'),
+	(35, 6, 18, '2021-05-29 23:22:02'),
+	(36, 6, 29, '2021-05-29 23:22:20');
 /*!40000 ALTER TABLE `ormeggi` ENABLE KEYS */;
 
 -- Dump della struttura di tabella charternautico.personale
@@ -284,12 +373,12 @@ INSERT INTO `porti` (`iDPorto`, `citta`, `nome`, `provincia`, `nTel`, `indirizzo
 
 -- Dump della struttura di tabella charternautico.prenotazioninoleggi
 CREATE TABLE IF NOT EXISTS `prenotazioninoleggi` (
-  `iDPrenotazione` varchar(5) NOT NULL,
+  `iDPrenotazione` smallint NOT NULL AUTO_INCREMENT,
   `codDocumento` varchar(16) DEFAULT NULL,
   `iDImb` tinyint(4) DEFAULT NULL,
-  `dataPrenotazione` datetime DEFAULT NULL,
-  `inizioNoleggio` datetime DEFAULT NULL,
-  `fineNoleggio` datetime DEFAULT NULL,
+  `dataPrenotazione` date DEFAULT NULL,
+  `inizioNoleggio` date DEFAULT NULL,
+  `fineNoleggio` date DEFAULT NULL,
   `caparra` varchar(4) DEFAULT NULL,
   `noleggiata` enum('SI','NO') DEFAULT NULL,
   PRIMARY KEY (`iDPrenotazione`),
@@ -297,14 +386,14 @@ CREATE TABLE IF NOT EXISTS `prenotazioninoleggi` (
   KEY `iDImb` (`iDImb`),
   CONSTRAINT `prenotazioninoleggi_ibfk_1` FOREIGN KEY (`codDocumento`) REFERENCES `clienti` (`codDocumento`),
   CONSTRAINT `prenotazioninoleggi_ibfk_2` FOREIGN KEY (`iDImb`) REFERENCES `imbarcazioni` (`iDImb`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- Dump dei dati della tabella charternautico.prenotazioninoleggi: ~3 rows (circa)
 /*!40000 ALTER TABLE `prenotazioninoleggi` DISABLE KEYS */;
 INSERT INTO `prenotazioninoleggi` (`iDPrenotazione`, `codDocumento`, `iDImb`, `dataPrenotazione`, `inizioNoleggio`, `fineNoleggio`, `caparra`, `noleggiata`) VALUES
-	('1', '2727009', 1, '2021-05-01 10:37:16', '2021-05-13 10:37:32', '2021-05-23 10:37:37', '1000', 'SI'),
-	('2', 'AB1275467', 6, '2021-05-10 10:39:36', '2021-05-14 10:39:31', '2021-05-17 10:38:24', '500', 'NO'),
-	('3', 'HPFPYM47A27D408H', 2, '2021-04-30 10:40:57', '2021-05-10 10:41:12', '2021-05-12 10:41:25', '700', 'SI');
+	(1, '2727009', 1, '2021-05-01 10:37:16', '2021-05-13 10:37:32', '2021-05-23 10:37:37', '1000', 'SI'),
+	(2, 'AB1275467', 6, '2021-05-10 10:39:36', '2021-05-14 10:39:31', '2021-05-17 10:38:24', '500', 'NO'),
+	(3, 'HPFPYM47A27D408H', 2, '2021-04-30 10:40:57', '2021-05-10 10:41:12', '2021-05-12 10:41:25', '700', 'SI');
 /*!40000 ALTER TABLE `prenotazioninoleggi` ENABLE KEYS */;
 
 -- Dump della struttura di tabella charternautico.qualifiche
